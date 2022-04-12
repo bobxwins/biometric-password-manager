901251a0cf8296ea1d7b866cd7b28ee26460fdb7
bob <bobwinsx@gmail.com>
2022-04-12 12:50:50 +0200

    login

---
+++ app/src/main/java/sample/LoginController.java
@@ -0,0 +1,15 @@
+package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+public class LoginController {
+
+        @FXML
+        private Button btnLoad;
+
+        @FXML
+        void loadEntry(ActionEvent event) {
+
+        }
+
+    }
--- app/src/main/java/sample/Main.java
+++ app/src/main/java/sample/Main.java
@@ -21,11 +21,19 @@ public class Main extends Application   {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        EntryController entryController = new EntryController();
+        //EntryController entryController = new EntryController();
+        LoginController loginController = new LoginController();
         FXMLLoader loader = new FXMLLoader();
-       loader.setController(entryController);
-      String ProjectPath = System.getProperty("user.dir")+"/pmlayerAuthenticated.fxml";
-       loader.setLocation(new URL(   "file:///"+ProjectPath));
+       loader.setController(loginController);
+      String ProjectPath = System.getProperty("user.dir")+"/login.fxml";
+      // String ProjectPath = System.getProperty("user.dir")+"/pmlayerAuthenticated.fxml";
+      loader.setLocation(new URL(   "file:///"+ProjectPath));
+
+        //loader.setLocation(getClass().getResource("/FXML/pmlayerAuthenticated.fxml"));
+
+       /* String test = Main.class.getResource("/FXML/pmlayerAuthenticated.fxml").getFile();
+        String test2= Main.class.getResource("/FXML/pmlayerAuthenticated.fxml").toString();
+        System.out.println(test+test2); */
        anchorPane = loader.<AnchorPane>load();
 
         Scene scene = new Scene( anchorPane);
